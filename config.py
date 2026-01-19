bed6263e0725c6b911bdc8f1c96984eb63b6cd6e
Lebouse <xaneop@gmail.com>
2026-01-20 00:19:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 # config.py
 
 import os
+from pytz import timezone
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 AUTHORIZED_USER_IDS = {int(x.strip()) for x in os.environ["AUTHORIZED_USER_IDS"].split(",")}
-DATABASE_PATH = "/data/scheduled_messages.db"  # ← данные будут в volume
-TIMEZONE = os.getenv("TIMEZONE", "UTC")
+DATABASE_PATH = "/data/scheduled_messages.db"
+TIMEZONE = timezone(os.getenv("TIMEZONE", "UTC"))
+WEB_API_SECRET = os.environ.get("WEB_API_SECRET")
