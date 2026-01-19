13a9e28e83134941e7716de2f5daed6c7d86d8b6
Lebouse <xaneop@gmail.com>
2026-01-20 00:58:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-# config.py
-
 import os
 from pytz import timezone
 
@@ -8,3 +6,4 @@ AUTHORIZED_USER_IDS = {int(x.strip()) for x in os.environ["AUTHORIZED_USER_IDS"]
 DATABASE_PATH = "/data/scheduled_messages.db"
 TIMEZONE = timezone(os.getenv("TIMEZONE", "UTC"))
 WEB_API_SECRET = os.environ.get("WEB_API_SECRET")
+ADMIN_SECRET = os.environ.get("ADMIN_SECRET")
